ae1e2a272d9567e501486db19957df6613a675e1
Joe Bateson <jbateson@yelp.com>
2020-06-08 03:46:48 -0700

    clean up a bit, fixed some stuff

--- src/background/index.js
+++ src/background/index.js
@@ -2,44 +2,19 @@
 const { createFFmpeg } = require('@ffmpeg/ffmpeg');
 
 const corePath = global.chrome.runtime.getURL('./vendor/ffmpeg-core/ffmpeg-core.js');
-console.log('hmmm', { corePath });
-
-// const corePath = require.resolve('@ffmpeg/core');
-// console.log({corePath});
-
 const ffmpeg = createFFmpeg({ corePath, log: true });
 
 const playlists = new Map();
 
-// global.chrome.webNavigation.onHistoryStateUpdated.addListener((data) => {
-//     console.log('wooo', { data });
-//     playlists.clear();
-//     console.log('cleared playlists map', playlists);
-//     global.chrome.tabs.sendMessage(data.tabId, { event: 'changed_tab', url: data.url });
-// });
-
 global.chrome.webRequest.onCompleted.addListener(
     function (info) {
-        ffmpeg.load();
-        console.log('Cat intercepted: ' + info.url, { info });
-        if (info.tabId > 0) {
-            // the request might be from our own extension
-            global.chrome.tabs.sendMessage(info.tabId, { event: 'got_playlist', url: info.url });
-        }
-        // const urlParts = info.url.split('/');
-        // const quality = urlParts[urlParts.length - 2];
-        const videoId = info.url.match(/ext_tw_video\/(\d+?)\//)[1];
+        console.log('m3u8 playlist intercepted: ' + info.url, { info });
+        const videoId = info.url.match(/(?:amplify|ext_tw)_video\/(\d+?)\//)[1];
 
-        // let qualityMap = playlists.get(videoId);
-        // if (!qualityMap) {
-        //     qualityMap = new Map();
-        //     playlists.set(videoId, qualityMap);
-        // }
-
+        // TODO: work out good removal strategy to avoid this growing infinitely
         playlists.set(videoId, info.url);
         console.log(playlists);
     },
-    // filters
     {
         urls: ['https://video.twimg.com/*.m3u8?tag=*'],
     }
@@ -47,26 +22,55 @@ global.chrome.webRequest.onCompleted.addListener(
 
 global.chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.greeting === 'hello') {
-        const filename = `${request.tweetId}.mp4`;
+        const filename = `${request.videoId}.mp4`;
         transcode(request.videoId, filename)
             .then((url) => {
-                global.chrome.downloads.download({
-                    url: url,
-                    filename, // Optional
-                });
-                sendResponse('done!');
+                if (url) {
+                    global.chrome.downloads.download({
+                        url: url,
+                        filename,
+                    });
+                    sendResponse('done!');
+                } else {
+                    sendResponse('failed :(');
+                }
             })
             .catch((err) => sendResponse(err));
 
         return true;
+    } else if (request.event === 'contextmenu') {
+        if (!playlists.has(request.videoId)) {
+            sendResponse({error: 'missing_video_id'});
+        } else {
+            ffmpeg.load().then(() => sendResponse('loaded'));
+        }
     }
 });
 
 export const getLastValueInMap = (map) => Array.from(map)[map.size - 1][1];
 
+function getUrlFromData(data) {
+    const blob = new Blob([data.buffer], { type: 'video/mp4' });
+    const url = URL.createObjectURL(blob);
+    console.log({ url });
+    return url;
+}
+
 const transcode = async (videoId, filename) => {
     console.log('Loading ffmpeg-core.js');
+    // TODO: find good way to preload this - seems like background.js still janks up
+    // the main thread when we run this
     await ffmpeg.load();
+
+    try {
+        // Short-circuit in case we already have it
+        const data = ffmpeg.read(filename);
+        console.log('No need to transcode, already done:', filename)
+        return getUrlFromData(data);
+    } catch (e) {
+        // nada
+    }
+
     console.log('Start transcoding');
 
     const masterPlaylist = playlists.get(videoId);
@@ -110,14 +114,16 @@ const transcode = async (videoId, filename) => {
     );
 
     await ffmpeg.writeText('input.m3u8', playListData);
-    await ffmpeg.run(
-        `-protocol_whitelist file,http,https,tcp,tls -i input.m3u8 -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 ${filename}`
-    );
+    try {
+        await ffmpeg.run(
+            `-protocol_whitelist file,http,https,tcp,tls -i input.m3u8 -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 ${filename}`
+        );
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+
     console.log('Complete transcoding');
     const data = ffmpeg.read(filename);
-
-    const blob = new Blob([data.buffer], { type: 'video/mp4' });
-    const url = URL.createObjectURL(blob);
-    console.log({ url });
-    return url;
+    return getUrlFromData(data);
 };
--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,176 +1,103 @@
-// If your extension doesn't need a content script, just leave this file empty
-
-// This is an example of a script that will run on every page. This can alter pages
-// Don't forget to change `matches` in manifest.json if you want to only change specific webpages
-// printAllPageLinks();
-
-// // This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
-// export function printAllPageLinks() {
-//   const allLinks = Array.from(document.querySelectorAll('a')).map(
-//     link => link.href
-//   );
-
-//   console.log('-'.repeat(30));
-//   console.log(
-//     `These are all ${allLinks.length} links on the current page that have been printed by the Sample Create React Extension`
-//   );
-//   console.log(allLinks);
-//   console.log('-'.repeat(30));
-// }
-
-// console.log('hi!!!! lol');
-// document.querySelector('video').addEventListener('click', (e) => {
-//     console.log('lol', {e});
-// });
-
-// import downloadSvg from '../zondicons/download.svg';
-
-global.chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log(request.event, { request, sender });
-    const urlPieces = window.location.href.split('/');
-    if (urlPieces[urlPieces.length - 2] === 'status') {
-        addDownloadButton();
+document.addEventListener('contextmenu', (e) => {
+    const placementTracking = e.target.closest('[data-testid="placementTracking"]');
+    if (placementTracking) {
+        const video = placementTracking.querySelector('video');
+        if (video) {
+            console.log({ video, placementTracking });
+            handleVideoRightClick(video, placementTracking);
+        }
     }
 });
 
-// console.log(downloadSvg);
+function stop(e) {
+    console.log('stopping', e);
+    e.preventDefault();
+    e.stopPropagation();
+}
 
-let attempts = 0;
-
-function addDownloadButton() {
-    if (attempts >= 5) {
+function handleVideoRightClick(video, placementTracking) {
+    if (video.dataset.dlButtonDone) {
         return;
     }
 
-    console.log('trying', { attempts });
+    if (video.src.endsWith('.mp4')) {
+        // it's a gif, funnily enough
+        // TODO: support gifs
+        return;
+    }
 
-    const group = document.querySelector('[role="group"]');
-    if (group) {
-        // const row = document.querySelector('article > div > div:nth-child(3) > div:nth-child(3)');
-        const row = document.querySelector(
-            'a[href="https://help.twitter.com/using-twitter/how-to-tweet#source-labels"]'
-        ).parentElement.parentElement.parentElement;
-        console.log({ row });
+    console.log({ video, poster: video.poster });
+    const videoId = video.poster.match(/\/(?:ext_tw|amplify)_video_thumb\/(\d+?)\//)[1];
+    console.log({ videoId, video });
 
+    var menuItemObserver = new MutationObserver(function (mutations, me) {
+        console.log({ mutations });
+        const menuItem = placementTracking.querySelector('[role=menuitem]');
+        console.log({ menuItem });
+        if (menuItem) {
+            const menuItemWrapper = menuItem.parentElement;
+            if (menuItemWrapper.querySelector('[data-dl-button]')) {
+                console.warn('already added button');
+                me.disconnect();
+                return;
+            }
 
-        const video = document.querySelector('video');
-        const videoId = video.poster.match(/\/ext_tw_video_thumb\/(\d+?)\//)[1];
-        console.log({ videoId, video });
-        const placementTracking = video.closest('[data-testid="placementTracking"]');
+            console.log('adding button');
 
-        placementTracking.addEventListener('contextmenu', (e) => {
-            var menuItemObserver = new MutationObserver(function (mutations, me) {
-                console.log({ mutations });
-                const menuItem = placementTracking.querySelector('[role=menuitem]');
-                console.log({ menuItem });
-                if (menuItem) {
-                    const menuItemWrapper = menuItem.parentElement;
-                    if (menuItemWrapper.querySelector('[data-dl-button]')) {
-                        console.warn('already added button');
-                        me.disconnect();
-                        return;
-                    }
+            const clone = menuItem.cloneNode(true);
+            clone.tabIndex = 1;
+            clone.dataset.dlButton = 'true';
+            const textSpan = clone.querySelector('span');
+            textSpan.innerText = 'Download video';
+            textSpan.style.opacity = 0.2;
 
-                    const clone = menuItem.cloneNode(true);
-                    clone.tabIndex = 1;
-                    clone.dataset.dlButton = 'true';
-                    const textSpan = clone.querySelector('span');
-                    textSpan.innerText = 'Download video';
+            clone.addEventListener('click', stop);
 
-                    clone.addEventListener('mouseover', (e) => {
-                        clone.style.backgroundColor = 'rgba(255, 255, 255, 0.03)';
-                    });
+            menuItem.insertAdjacentElement('afterend', clone);
 
-                    clone.addEventListener('mouseout', (e) => {
-                        clone.style.backgroundColor = 'transparent';
-                    });
+            global.chrome.runtime.sendMessage({ videoId, event: 'contextmenu' }, (request, sender, sendResponse) => {
+                console.log({ request });
 
-                    clone.addEventListener('click', (e) => {
-                        console.log('clicked download ' + videoId);
-                        const urlPieces = window.location.href.split('/');
-                        global.chrome.runtime.sendMessage(
-                            { videoId, tweetId: urlPieces[urlPieces.length - 1], greeting: 'hello' },
-                            function (response) {
-                                console.log('got response!');
-                                console.log({ response });
-                                textSpan.innerText = 'Downloaded!';
-                            }
-                        );
-                    });
-
-                    menuItem.insertAdjacentElement('afterend', clone);
-
-                    me.disconnect();
+                if (!request) {
                     return;
                 }
+
+                if (request.error === 'missing_video_id') {
+                    textSpan.style.color = 'red';
+                    console.error(
+                        request.error,
+                        'No video id found in background script data - try reloading the page'
+                    );
+                    return;
+                }
+
+                textSpan.style.opacity = 1;
+
+                clone.addEventListener('mouseover', (e) => {
+                    clone.style.backgroundColor = 'rgba(255, 255, 255, 0.03)';
+                });
+
+                clone.addEventListener('mouseout', (e) => {
+                    clone.style.backgroundColor = 'transparent';
+                });
+
+                clone.removeEventListener('click', stop);
+                clone.addEventListener('click', (e) => {
+                    console.log('clicked download ' + videoId);
+                    global.chrome.runtime.sendMessage({ videoId, greeting: 'hello' }, function (response) {
+                        console.log('got response!');
+                        console.log({ response });
+                    });
+                });
             });
 
-            menuItemObserver.observe(placementTracking, {
-                childList: true,
-                subtree: true,
-            });
-
-            console.log({ menuItemObserver });
-        });
-
-        if (row.querySelector('[data-download-link]')) {
-            console.warn('button already there');
+            me.disconnect();
             return;
         }
+    });
 
-        const aTagClasses = row.querySelector('a').classList;
-        const button = document.createElement('button');
-
-        // const img = new Image();
-        // img.src = downloadSvg;
-        // button.appendChild(img);
-
-        button.classList = aTagClasses;
-        button.dataset.downloadLink = true;
-
-        const span = document.createElement('span');
-        span.innerText = 'Download video';
-        button.appendChild(span);
-
-        button.addEventListener('click', () => {
-            span.innerText = 'Downloading...';
-            button.disabled = true;
-
-            const urlPieces = window.location.href.split('/');
-            global.chrome.runtime.sendMessage(
-                { tweetId: urlPieces[urlPieces.length - 1], greeting: 'hello' },
-                function (response) {
-                    console.log('got response!');
-                    console.log({ response });
-                    span.innerText = 'Downloaded!';
-                }
-            );
-
-            // global.chrome.downloads.download(
-            //     {
-            //         url: '#',
-            //         // filename: 'my-cool-video.mp4', // Optional
-            //     },
-            //     (downloadId) => {
-            //         global.chrome.downloads.pause(downloadId, () => {
-            //             console.log('paused!');
-            //             global.chrome.runtime.sendMessage({ greeting: 'hello', downloadId }, function (response) {
-            //                 console.log('got response!');
-            //                 console.log({ response });
-            //                 span.innerText = 'Downloaded!';
-            //             });
-            //         });
-            //     }
-            // );
-        });
-
-        const rowContents = row.children[0];
-        const clone = rowContents.children[0].cloneNode();
-        clone.appendChild(button);
-        rowContents.appendChild(clone);
-    } else {
-        console.warn('no group, trying again');
-        attempts++;
-        setTimeout(addDownloadButton, 500);
-    }
+    menuItemObserver.observe(placementTracking, {
+        childList: true,
+        subtree: true,
+    });
 }
